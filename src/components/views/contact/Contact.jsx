330963730708ec495ab52f87f410a3b0f61b61ea
Victor-python <64462520+Victor-python@users.noreply.github.com>
2022-11-05 16:35:46 +0200

    more less looking semigood

--- src/components/views/contact/Contact.jsx
+++ src/components/views/contact/Contact.jsx
@@ -5,40 +5,51 @@ const Contact = () => {
     
   return (
     <div className='contact view'>
-     
-     
+        <h2>Conctact us...</h2>
+        <h3>Victor:</h3>
+        <div className="oneSome">
+            <img src={someImages["mail"]} alt="some"  className="someLogo"/>
+            <a target="blank" href="victorgrinan@gmail.com">Mail</a>
+        </div>
         <div className="oneSome">
             <img src={someImages["instagram"]} alt="some"  className="someLogo"/>
-            <a href="">Instagram</a>
+            <a target="blank" href="https://www.instagram.com/victor_grinan_dev">Instagram</a>
         </div>
         <div className="oneSome">
             <img src={someImages["github"]} alt="some"  className="someLogo"/>
-            <a href="">Github</a>
+            <a target="blank" href="https://github.com/Victor-Grinan-Dev">Github</a>
         </div>
         <div className="oneSome">
             <img src={someImages["linkedin"]} alt="some"  className="someLogo"/>
-            <a href="">Linkedin</a>
+            <a target="blank" href="https://www.linkedin.com/in/victor-gri%C3%B1an">Linkedin</a>
         </div>
-        <div className="oneSome">
-            <img src={someImages["facebook"]} alt="some"  className="someLogo"/>
-            <a href="">Facebook</a>
-        </div>
-        <div className="oneSome">
-            <img src={someImages["youtube"]} alt="some"  className="someLogo"/>
-            <a href="">Youtube</a>
-        </div>
+        <h3>Vitali:</h3>
         <div className="oneSome">
             <img src={someImages["mail"]} alt="some"  className="someLogo"/>
-            <a href="">Mail</a>
+            <a target="blank" href="victorgrinan@gmail.com">Mail</a>
         </div>
         <div className="oneSome">
-            <img src={someImages["whatsapp"]} alt="some"  className="someLogo"/>
-            <a href="">Whatsapp</a>
+            <img src={someImages["github"]} alt="some"  className="someLogo"/>
+            <a target="blank" href="">Github</a>
         </div>
         <div className="oneSome">
-            <img src={someImages["twitter"]} alt="some"  className="someLogo"/>
-            <a href="">Twitter</a>
+            <img src={someImages["linkedin"]} alt="some"  className="someLogo"/>
+            <a target="blank" href="">Linkedin</a>
         </div>
+        <h3>Sonja :</h3>
+        <div className="oneSome">
+            <img src={someImages["mail"]} alt="some"  className="someLogo"/>
+            <a target="blank" href="">Mail</a>
+        </div>
+        <div className="oneSome">
+            <img src={someImages["github"]} alt="some"  className="someLogo"/>
+            <a target="blank" href="">Github</a>
+        </div>
+        <div className="oneSome">
+            <img src={someImages["linkedin"]} alt="some"  className="someLogo"/>
+            <a target="blank" href="">Linkedin</a>
+        </div>
+        
    
        
     </div>
@@ -46,3 +57,23 @@ const Contact = () => {
 }
 
 export default Contact;
+
+/*
+<div className="oneSome">
+            <img src={someImages["facebook"]} alt="some"  className="someLogo"/>
+            <a target="blank" href="">Facebook</a>
+        </div>
+        <div className="oneSome">
+            <img src={someImages["youtube"]} alt="some"  className="someLogo"/>
+            <a target="blank" href="">Youtube</a>
+        </div>
+ 
+        <div className="oneSome">
+            <img src={someImages["whatsapp"]} alt="some"  className="someLogo"/>
+            <a target="blank" href="">Whatsapp</a>
+        </div>
+        <div className="oneSome">
+            <img src={someImages["twitter"]} alt="some"  className="someLogo"/>
+            <a target="blank" href="">Twitter</a>
+        </div>
+*/
