72a8335623cd90b2a81f06b85cae7e22870c9bb8
Victor-python <64462520+Victor-python@users.noreply.github.com>
2022-11-05 17:22:56 +0200

    added a couple of emojis in the sidebar

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -2,15 +2,18 @@ import React from 'react';
 import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 const SideBar = () => {
-    const isLogged = useSelector(state => state.portal.isLogged)
+    const isLogged = useSelector(state => state.portal.isLogged);
+    const user = useSelector(state =>state.portal.currentUser);
+
   return (
     isLogged && <div className='sidebarContainer'>
         <p></p>
         <Link to="/">🏠</Link>
         <Link to="/profile">👤</Link>
         <p>💬</p>
-        <Link to="/createuser">+👤</Link>
-        <p>✍️</p>
+        {user.type === "admin" && <Link to="/adduser">+👤</Link>}
+        {user.type === "admin" && <Link to="/post">✍️</Link>}
+
         <Link to="/about">📜</Link>
         <Link to="/contact">☎️</Link>
         <Link to="/settings">⚙️</Link>
