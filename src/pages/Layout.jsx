77aed4b2717ebf7199830bea590daba413672369
Victor-python <64462520+Victor-python@users.noreply.github.com>
2022-11-05 07:24:59 +0200

    conditional index ok

---
+++ src/pages/Layout.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Main from '../components/Main';
+
+function Layout() {
+  return (
+    <layout>
+        <Main/>
+    </layout>
+  )
+}
+
+export default Layout;
