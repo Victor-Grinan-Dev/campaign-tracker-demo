922e41020c16f610895ea3b484eb02c71df10c59
Victor-python <64462520+Victor-python@users.noreply.github.com>
2022-11-05 07:33:13 +0200

    ready for work in it

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import Layout from './pages/Layout';
 import Home from './components/home/Home';
 import Login from './components/login/Login';
 
-const isLogged = false;
+const isLogged = true;
 
 function App() {
   return (
