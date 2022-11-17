395ae10180e8eda437d288e78d7fe12703ad4229
Victor-python <64462520+Victor-python@users.noreply.github.com>
2022-11-17 21:43:14 +0200

    side data first draft

--- src/components/myArmy/CreateFormation.jsx
+++ src/components/myArmy/CreateFormation.jsx
@@ -6,7 +6,6 @@ import { setFormationObj } from '../../features/formationSlice';
 import CreateUnit from './CreateUnit';
 import FormationCard from './FormationCard';
 
-
 const CreateFormation = () => {
 
   const nameFormation = useSelector(state => state.formation.name)
@@ -42,49 +41,47 @@ const CreateFormation = () => {
                     <option value="" hidden></option>
                   </select>
               </div>
-              
+              <div className="sideData">
+                <div className="keys">
+                  <p className='createFormInfoAtrr'>🕴Model count:</p>
+                  <p className='createFormInfoAtrr'>🪖Infantry count:</p>
+                </div>
+                <div className="values">
+                  <p className='createFormInfoAtrr'>{creatingFormation.model_count}</p>
+                  <p className='createFormInfoAtrr'>{creatingFormation.infantry_count}</p>
+                </div>
+              </div>
+
+              {/*
+              <div className="sideData">
+                <div className="keys">
+                <p className='createFormInfoAtrr'>⚒️work force</p>
+                <p className='createFormInfoAtrr'>👀vision</p>
+                <p className='createFormInfoAtrr'>🧠intelligence</p>
+                <p className='createFormInfoAtrr'>🎒carry capacity</p>
+                
+                </div>
+                <div className="values">
+                <p className='createFormInfoAtrr'>{creatingFormation.work_force}</p>
+                <p className='createFormInfoAtrr'>{creatingFormation.vision}</p>
+                <p className='createFormInfoAtrr'>{creatingFormation.intelligence}</p>
+                <p className='createFormInfoAtrr'>{creatingFormation.carry_capacity}</p>
+                </div>
+              </div>
+              */}
+
                 {/* 
-                ⚔️🥾
-                action_points(pin):1
-                ⚒️ work_force(pin):0
-                🗡️🎯💥☠️damage(pin):null
-                🛡️defense(pin):0
-                model_count(pin):0
-                👀🔭vision(pin):1
-                Xp(pin):0
-                actions(pin):
-                🧠🎓intelligence(pin):1
-                📊level(pin):0
-                🎁benefits(pin):
-                🏅badges(pin):
-                🥾movement(pin):100
-                maxMovement(pin):100
-                📚dedication(pin):
+                ⚔️🥾🗡️🎯💥☠️🛡️🔭🎓📚📈🧮🏴󠁧󠁢󠁥󠁮󠁧󠁿🚩📊🎁🏅
+                faction(pin):""
                 color(pin):"white"
+
+                subfaction(pin):""
                 subColor(pin):"white"
+
                 is_listed(pin):false
-                📈point_cost(pin):0
-                🎒carry_capacity(pin):0
-                🕴🖩🧮infantry_count(pin):0
-                isBeen(pin):false
-                isMoved(pin):false
-                name(pin):"-"
-                composition(pin):
+
                 s_description(pin):""
                 l_description(pin):""
-                🏴󠁧󠁢󠁥󠁮󠁧󠁿🚩faction(pin):""
-                subfaction(pin):""
-
-
-                <p className='createFormInfoAtrr'> work_force: {creatingFormation.work_force} </p>
-                <p className='createFormInfoAtrr'> model_count: {creatingFormation.model_count} </p>
-                <p className='createFormInfoAtrr'> actions: { creatingFormation.actions ? creatingFormation.actions : "-"} </p>
-                <p className='createFormInfoAtrr'> benefits: { creatingFormation.benefits ? creatingFormation.benefits : "-"} </p>
-                <p className='createFormInfoAtrr'> movement: {creatingFormation.movement === 100 ? 0 :creatingFormation.movement} </p>
-                <p className='createFormInfoAtrr'> maxMovement: {creatingFormation.maxMovement === 100 ? 0 :creatingFormation.maxMovement} </p>
-                <p className='createFormInfoAtrr'> dedication: {creatingFormation.dedication} </p>
-                <p className='createFormInfoAtrr'> carry_capacity: {creatingFormation.carry_capacity} </p>
-                <p className='createFormInfoAtrr'> infantry_count: {creatingFormation.infantry_count} </p>
                 */}
             </div>
 
